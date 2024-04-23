e3d00a79357e91232bba3e4a7a311f638928721d
daniel-pohl <danielpohl@mail.de>
2024-04-23 11:14:08 +0200

    second commit

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,20 +4,25 @@ public class Car {
     String model;
     String color;
     int yearOfManufacture;
+    int speed;
 
     public Car() {
-
     }
 
-    public Car(String brand, String model, String color, int yearOfManufacture) {
+    public Car(String brand, String model, String color, int yearOfManufacture, int speed) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.yearOfManufacture = yearOfManufacture;
+        this.speed = speed;
     }
-
 
     public void start() {
         System.out.println("The " + color + " " + brand + " " + model + " has started.");
+    }
+
+    public void accelerate(int acceleration) {
+        speed += acceleration;
+        System.out.println("The car is accelerating. Current speed: " + speed + " km/h");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car myCar = new Car("BMW", "3er", "Black", 2022);
+        Car myCar1 = new Car("BMW", "3er", "Black", 2022, 230);
+        myCar1.start();
 
-
-        myCar.start();
-
+        Car myCar2 = new Car("BMW", "1er", "Orange", 2015, 60);
+        myCar2.accelerate(10);
     }
 
 }
